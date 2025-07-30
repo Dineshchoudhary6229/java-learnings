ecadb7769188d97d533013e519e6bc6df7704ed2
Dineshchoudhary6229 <Dkdinesh6229@gmail.com>
2025-07-30 17:31:11 +0530

    Equals method and equalsIgnoreCase method

---
+++ java-programmes/src/StringComparisionEqual_Method.java
@@ -0,0 +1,19 @@
+public class StringComparisionEqual_Method
+{
+    public static void main(String[] args)
+    {
+      String str1 = "Ram";
+      String str2 = "Ram";
+      String str3 = "rAm";
+      boolean equalObj = str1.equals(str2);
+           System.out.println(equalObj);
+           System.out.println(str1.equals(str3));
+      String str4 = new String("Ram");   // String with new keyword having stored in heap memory not in String constant pool
+           System.out.println(str1.equals(str4));
+      String str5 = "Ravan";
+      String str6 = "raVan";
+      boolean str7 = str5.equalsIgnoreCase(str6);  // equalsIgnoreCase method ignore case-sensitive of strings
+           System.out.println(str7);
+
+    }
+}
