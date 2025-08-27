018f380d9bba0047122ab745bfcd47d53996db43
Dineshchoudhary6229 <Dkdinesh6229@gmail.com>
2025-08-27 13:54:25 +0530

    Indentation

--- java-programmes/src/AnnyomousArray.java
+++ java-programmes/src/AnnyomousArray.java
@@ -2,12 +2,12 @@
 
 public class AnnyomousArray
 {
- static void display(int b[])
+ static void display(int []b)
  {
-  for (int i: b)                                                                      //for(int i=0; i<b.length; i++)
-  System.out.println("Passing annymous array directly to the method :" + i);       // sout(b[i]);
- }
-public static void main(String args[])
+  for (int i: b)     {                                                                 //for(int i=0; i<b.length; i++)
+   System.out.println("Passing annymous array directly to the method :" + i);       // sout(b[i]);
+ }}
+public static void main(String []args)
 {
  display(new int[]{4,5,18,63});
 }
